4dce91666056282e8d9936c27506dbfe4163dcca
mattjagiello <mjagiello@gmail.com>
2020-03-05 17:40:43 -0600

    Modified review delete redirect

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -35,7 +35,7 @@ class ReviewsController < ApplicationController
   def destroy
     @review = Review.find(params[:id])
     @review.destroy
-    redirect_to reviews_path
+    redirect_to user_path(@review[:user_id])
   end
 
   private
